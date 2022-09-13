5ec487c842226dad36927430ccc91371d20902a9
Hakim Razalan <akimrr@gmail.com>
2022-09-13 13:20:54 +0800

    add comments controller

---
+++ app/controllers/comments.js
@@ -0,0 +1,23 @@
+const index = (async (req, res) => {
+	res.status(200);
+	res.send("Testing");
+});
+
+const create = ((req, res) => {
+	//
+});
+
+const update = ((req, res) => {
+	//
+});
+
+const destroy = ((req, res) => {
+	//
+});
+
+module.exports = {
+	index,
+	create,
+	update,
+	destroy
+};
