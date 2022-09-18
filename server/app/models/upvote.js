d37770aef0151a590a063c86fa27d3c439b989d0
Hakim Razalan <akimrr@gmail.com>
2022-09-18 11:50:28 +0800

    create upvotes models, controller, migrations

---
+++ server/app/models/upvote.js
@@ -0,0 +1,17 @@
+const { toPascalCase } = require('js-convert-case');
+const bookshelf = require('@load/bookshelf');
+const name = require('path').parse(__filename).name;
+
+require('@models/user');
+require('@models/comment');
+
+const upvote = require('@load/models')(name, {
+	user() {
+		return this.belongsTo('User');
+	},
+	comment() {
+		return this.belongsTo('Comment');
+	}
+});
+
+module.exports = bookshelf.model(toPascalCase(name), upvote);
---
+++ server/database/migrations/20220918033715_create_upvotes_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("upvotes", (table) => {
+    table.increments("id");
+    table.bigInteger("user_id");
+    table.bigInteger("comment_id");
+    table.timestamps();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("upvotes");
+};
