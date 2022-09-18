ebe1189ee672fe32c290b5dc2627eff27b279401
Hakim Razalan <akimrr@gmail.com>
2022-09-18 11:24:30 +0800

    add ghosty command list. add env_file config

--- server/config/general.js
+++ server/config/general.js
@@ -3,6 +3,7 @@ const path = require('path');
 const server = _.endsWith(process.cwd(), '/server') ? path.resolve(process.cwd()) : path.resolve(process.cwd(), 'server/');
 
 module.exports = {
+	env_file: path.resolve(server + '/.env'),
 	knex_file: path.resolve(server + '/knexfile.js'),
 	migration_directory: path.resolve(server + '/database/migrations'),
 	seed_directory: path.resolve(server + '/database/seeds')
--- server/core/cli/ghosty.js
+++ server/core/cli/ghosty.js
@@ -1,23 +1,61 @@
 #! /usr/bin/env node
 require('module-alias/register');
+const _ = require("lodash");
+var clc = require("cli-color");
 
 let args = process.argv.slice(2);
 const operationMapping = {
-	'make:model': './operations/make-model.js',
-	'make:controller': './operations/make-controller.js',
-	'make:migration': './operations/make-migration.js',
-	'db:migrate': './operations/db-migrate.js',
-	'db:seed': './operations/db-seed.js',
+	'make:model': {
+		description: 'Create new model - make:model <name>',
+		file: './operations/make-model.js'
+	},
+	'make:controller': {
+		description: 'Create new controller - make:controller <name>',
+		file: './operations/make-controller.js'
+	},
+	'make:migration': {
+		description: 'Create new migration - make:migration <name>',
+		file: './operations/make-migration.js'
+	},
+	'db:migrate': {
+		description: 'Migrate latest migrations',
+		file: './operations/db-migrate.js'
+	},
+	'db:seed': {
+		description: 'Seed database',
+		file: './operations/db-seed.js'
+	},
 };
 
 const operation = args.shift();
 
 const run = () => {
+	if (_.isEmpty(operation)
+		|| operation == '-h'
+		|| operation == '--help') {
+		/** Show help */
+
+		let keyDescription = []
+		const commandHighlight = clc.whiteBright.bgXterm(196);
+		const allKeys = Object.keys(operationMapping).map((key) => {
+			keyDescription.push([
+				`${commandHighlight(key)}`,
+				`\t\t${operationMapping[key].description}`
+			]);
+		})
+
+		console.log('👻 Available Ghosty commands')
+		process.stdout.write(
+			clc.columns(keyDescription, { sep: '' })
+		  );
+		return;
+	}
+
 	if (!operationMapping[operation]) {
 		console.error('Invalid operation - '+operation);
 		return;
 	}
-	const runOperation = require(operationMapping[operation])(args);
+	const runOperation = require(operationMapping[operation].file)(args);
 }
 
 run();
