1de903f9302163d09b36a6c11c2a8d6a17539a69
Hakim Razalan <akimrr@gmail.com>
2022-09-19 12:28:28 +0800

    rearrange loader

--- server/loader.js
+++ server/loader.js
@@ -3,10 +3,10 @@ const _ = require("lodash");
 const path = require("path");
 const config = require("@config/general");
 
-/** Load ORM */
-require("@load/bookshelf");
-
 /** dot env */
 require("dotenv").config({
   path: config.env_file,
 });
+
+/** Load ORM */
+require("@load/bookshelf");
