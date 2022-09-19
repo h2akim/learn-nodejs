1a7643130b0300085b47938a2d696c4fe66be4f3
Hakim Razalan <akimrr@gmail.com>
2022-09-19 14:32:57 +0800

    broadcast event when new comment added / upvotes update

--- server/app/controllers/comment.js
+++ server/app/controllers/comment.js
@@ -1,6 +1,7 @@
 /** Comment Controller */
 const Comment = require("@models/comment");
 const { knex } = require("@load/bookshelf");
+const ticker = require("../channels/ticker");
 
 const index = async (req, res) => {
   const comment = await Comment.query((query) => {
@@ -21,29 +22,59 @@ const index = async (req, res) => {
   res.json(comment);
 };
 
-const store = async (req, res) => {
-	const user = req.header('User-Id');
-	const { comment, reply_to: replyTo } = req.body;
+const show = async (req, res) => {
+  const { commentId } = req.params;
 
-	let savingData = {
-		user_id: user,
-		comment: comment,
-	}
-
-	if (replyTo) {
-		savingData['parent_id'] = replyTo;
-	}
-
-	try {
-		const newComment = await Comment.forge().save(savingData);
-		res.json(newComment)
-	} catch (err) {
-		res.status(500).send('Whoops! Looks like error was on our end.');
-	}
+  const comment = await Comment.query((query) => {
+    query
+      .select([
+        "*",
+        knex("upvotes")
+          .where("upvotes.comment_id", knex.raw("??", "comments.id"))
+          .count()
+          .as("upvote_count"),
+      ])
+      .where("id", commentId)
+      .orderBy("created_at", "desc")
+      .orderBy("upvote_count", "desc");
+  }).fetchAll({
+    withRelated: ["user", "replies.user"],
+  });
+  res.json(comment);
 };
 
+const store = async (req, res) => {
+  const user = req.header("User-Id");
+  const { comment, reply_to: replyTo } = req.body;
+
+  let savingData = {
+    user_id: user,
+    comment: comment,
+  };
+
+  if (replyTo) {
+    savingData["parent_id"] = replyTo;
+  }
+
+  try {
+    const newComment = await Comment.forge().save(savingData);
+    broadcastNewComment(newComment);
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).send("Whoops! Looks like error was on our end.");
+  }
+};
+
+const broadcastNewComment = (comment) => {
+  const newData = {
+    id: comment.get("id"),
+    parent_id: comment.get("parent_id"),
+  };
+  ticker.broadcast(newData, "comment-update");
+};
 
 module.exports = {
   index,
-  store
+  store,
+  show,
 };
--- server/app/controllers/upvote.js
+++ server/app/controllers/upvote.js
@@ -1,9 +1,45 @@
 /** Upvote Controller */
+const Upvote = require("@models/upvote");
+const ticker = require("../channels/ticker");
 
-const index = async (req, res) => {
-  //
+const store = async (req, res) => {
+  const user = req.header("User-Id");
+  const { id, parent_id } = req.body;
+
+  const upvote = await Upvote.query((query) => {
+    query.where("comment_id", id).where("user_id", user);
+  }).fetch({ require: false });
+
+  if (!upvote) {
+    const savingData = {
+      comment_id: id,
+      user_id: user,
+    };
+    await Upvote.forge().save(savingData);
+  } else {
+    await Upvote.query((query) => {
+      query.where("comment_id", id).where("user_id", user);
+    }).destroy({ require: false });
+  }
+
+  broadcastNewUpvote({
+    id: id,
+    parent_id: parent_id,
+  });
+
+  res.json({
+    success: true,
+  });
+};
+
+const broadcastNewUpvote = (comment) => {
+  const newData = {
+    id: comment.id,
+    parent_id: comment.parent_id,
+  };
+  ticker.broadcast(newData, "comment-update");
 };
 
 module.exports = {
-  index,
+  store,
 };
