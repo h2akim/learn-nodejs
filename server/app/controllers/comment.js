39da92eab793f406fa5f4bb9e3bc318a288d26bd
Hakim Razalan <akimrr@gmail.com>
2022-09-19 12:30:32 +0800

    implement comment list and comment store endpoint

--- server/app/controllers/comment.js
+++ server/app/controllers/comment.js
@@ -1,28 +1,49 @@
 /** Comment Controller */
 const Comment = require("@models/comment");
+const { knex } = require("@load/bookshelf");
 
 const index = async (req, res) => {
-  const comment = await Comment.where("parent_id", null).fetchAll({
+  const comment = await Comment.query((query) => {
+    query
+      .select([
+        "*",
+        knex("upvotes")
+          .where("upvotes.comment_id", knex.raw("??", "comments.id"))
+          .count()
+          .as("upvote_count"),
+      ])
+      .where("parent_id", null)
+      .orderBy("created_at", "desc")
+      .orderBy("upvote_count", "desc");
+  }).fetchAll({
     withRelated: ["user", "replies.user"],
   });
   res.json(comment);
 };
 
-const store = (req, res) => {
-  //
+const store = async (req, res) => {
+	const user = req.header('User-Id');
+	const { comment, reply_to: replyTo } = req.body;
+
+	let savingData = {
+		user_id: user,
+		comment: comment,
+	}
+
+	if (replyTo) {
+		savingData['parent_id'] = replyTo;
+	}
+
+	try {
+		const newComment = await Comment.forge().save(savingData);
+		res.json(newComment)
+	} catch (err) {
+		res.status(500).send('Whoops! Looks like error was on our end.');
+	}
 };
 
-const update = (req, res) => {
-  //
-};
-
-const destroy = (req, res) => {
-  //
-};
 
 module.exports = {
   index,
-  store,
-  update,
-  destroy,
+  store
 };
